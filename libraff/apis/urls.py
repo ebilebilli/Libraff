aa3f6283239865fe29dc091f48737739b84ca563
Əbil Abilli <ebilebilli3@gmail.com>
2025-02-17 01:42:14 +0400

    First steps for library site logic

--- libraff/apis/urls.py
+++ libraff/apis/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 app_name = 'apis'
 urlpatterns = [path('books/', BookListAPIView.as_view(), name='books'),
-               path('book/<int:book_id>/', BookDetailAPIView.as_view(), name='book-details')
+               path('book/<int:book_id>/', BookDetailAPIView.as_view(), name='book-details'),
+               path('book/delete/<int:book_id>/', BookDeleteAPIView.as_view(), name='book-delete'),
+               path('book/update/<int:book_id>/', BookUpdateAPIView.as_view(), name='book-update')
      
 ]
--- libraff/apis/views.py
+++ libraff/apis/views.py
@@ -16,10 +16,27 @@ class BookListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
+    
+  
 class BookDetailAPIView(APIView):
     def get(self, request, book_id):
-        book = get_object_or_404(Book, book_id)
+        book = get_object_or_404(Book, id=book_id)
         serializer = BookSerializer(book)
         return Response(serializer.data)
+
+
+class BookDeleteAPIView(APIView):
+    def delete(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        book.delete()
+        return Response({'detail': 'Book deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class BookUpdateAPIView(APIView):
+    def put(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        serializer = BookSerializer(book, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
