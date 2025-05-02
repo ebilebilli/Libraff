92f4ea77277b8a3c0eee9ed4a503a66eb73627d9
Əbil Abilli <ebilebilli3@gmail.com>
2025-05-03 00:08:58 +0400

    Optimize URL structure

--- libraff/apis/urls.py
+++ libraff/apis/urls.py
@@ -9,43 +9,41 @@ from .favorite_apis import *
 app_name = 'apis'
 
 urlpatterns = [
-    #User endpoints:
+    # İstifadəçi endpoint-ləri:
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('users/', UserListAPIView.as_view(), name='users'),
     path('user/<int:user_id>/', AccountDetailAPIView.as_view(), name='user-account'),
 
-    #Book endpoints:
+    # Kitab endpoint-ləri:
     path('categories/', BookCategoryListAPIView.as_view(), name='categories'),
     path('categories/<int:category_id>/books/', BookListForCategoryAPIView.as_view(), name='category-books'),
     path('books/', BookListAPIView.as_view(), name='books'),
     path('book/<int:book_id>/', BookDetailAPIView.as_view(), name='book-detail'),
-    path('book/<int:book_id>/comments/', CommentManagementAPIView.as_view(), name='comment-management'),
+    path('book/<int:book_id>/comments/', CommentManagementAPIView.as_view(), name='manage-comments'),
     path('book/<int:book_id>/download/', BookDownloadAPIView.as_view(), name='download-book'),
     path('books/search/', BookSearchAPIView.as_view(), name='book-search'),
     path('books/filter/', BookFilterAPIView.as_view(), name='book-filter'),
 
-    #Interaction endpoints:
+    # İnteraksiya endpoint-ləri:
     path('book/<int:book_id>/comments/', CommentsForBookAPIView.as_view(), name='comments-for-book'),
     path('book/<int:book_id>/comment/create/', CreateCommentAPIView.as_view(), name='create-comment'),
-    path('book/<int:book_id>/comments/manage/', CommentManagementAPIView.as_view(), name='comment-management'),
-    path('book/<int:comment_id>/comment/detail/', CommentDetailAPIView.as_view(), name='comment-detail'),
+    path('book/<int:book_id>/comment/<int:comment_id>/detail/', CommentDetailAPIView.as_view(), name='comment-detail'),
     path('user/<int:user_id>/comments/', CommentListForUserAPIView.as_view(), name='user-comments'),
     path('book/<int:book_id>/likes/', LikeListForBookAPIView.as_view(), name='likes-for-book'),
     path('comment/<int:comment_id>/likes/', LikeListForCommentAPIView.as_view(), name='likes-for-comment'),
-    path('book/<int:book_id>/like/manage/', LikeManagementForBookAPIView.as_view(), name='like-management-for-book'),
-    path('book/<int:comment_id>/like/manage/', LikeManagementForCommentAPIView.as_view(), name='like-management-for-comment'),
+    path('book/<int:book_id>/like/manage/', LikeManagementForBookAPIView.as_view(), name='manage-like-for-book'),
+    path('comment/<int:comment_id>/like/manage/', LikeManagementForCommentAPIView.as_view(), name='manage-like-for-comment'),
 
-    #Favorite endpoints:
+    # Favorit endpoint-ləri:
     path('user/<int:user_id>/favorites/open/', OpenFavoriteListForUserAPIView.as_view(), name='open-favorites-for-user'),
-    path('user/<int:user_id>/favorites/private/', PrivateFavoriteListForUserAPIView.as_view(), name='open-favorites-for-user'),
+    path('user/<int:user_id>/favorites/private/', PrivateFavoriteListForUserAPIView.as_view(), name='private-favorites-for-user'),
     path('favorite/<int:favorite_id>/', FavoriteDetailAPIView.as_view(), name='favorite-detail'),
     path('book/<int:book_id>/favorite/create/', CreateFavoriteAPIView.as_view(), name='create-favorite'),
-    path('favorite/<int:favorite_id>/manage/', FavoriteManagementAPIView.as_view(), name='favorite-management'),
+    path('favorite/<int:favorite_id>/manage/', FavoriteManagementAPIView.as_view(), name='manage-favorite'),
 
-    #JWT endpoints: 
+    # JWT endpoint-ləri:
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-               
 ]
